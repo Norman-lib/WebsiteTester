6a649b830a5aa077235e0fb608212c44671ed065
OnsChahloul <hamzamajidtmnttmnt@gmail.com>
2023-02-09 15:15:15 +0100

    installing the libriary UniTask

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -1,40 +1,81 @@
+
+using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using TMPro;
 using System.Net;
-using UnityEngine;
 using UnityEngine.Networking;
-using UniTask;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Events;
+
 
 public class ButtonController : MonoBehaviour
 {
+    //les boutons de controle
+    public Button startButton;
+    public Button stopButton;
+    public Button errorButton;
 
+    UnityAction actionStart;
+    UnityAction actionStop;
+    UnityAction actionError;
 
-    async UniTask TestWebsiteResponse()
+    
+
+    string[] websites1 =
     {
-        using (var request = UnityWebRequest.Get("https://www.example.com"))
-        {
-            await request.SendWebRequest().WaitUntilDone();
+        "www.amazon.fr",
+        "www.google.fr",
+        "https://drivronlinebackendtest.azurewebsites.net/api/v1/Applications",
+    };
+    string [] websites2 = { "https://drivronlinebackendtest.azurewebsites.net/api/v1/Applications",
+        "www.microsoft.fr",
+        "drivr.online",
+        "www.sfr.fr",
+    };
+    string [] websites3 = { 
+        "www.easyjet.fr",
+        "https://drivronlinebackendtest.azurewebsites.net/api/v1/ApplicationConfiguration/parameters",
+        "https://github.com/picoxr/support",
+    };
+    async UniTask TestWebsiteResponse(string[] webs)
+    {
+        var progress = Progress.Create<float>(x => Debug.Log(x));
 
-            if (request.isNetworkError || request.responseCode != (long)HttpStatusCode.OK)
+        var requestt = await UnityWebRequest.Get("www.amazon.fr")
+            .SendWebRequest()
+            .ToUniTask(progress: progress);
+
+        using (var request = UnityWebRequest.Get("www.amazon.fr"))
+        {
+            await request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.responseCode != (long)HttpStatusCode.OK)
             {
-                Debug.LogError("Request failed: " + request.error);
-            }
-            else
+                Debug.Log("Request Failed: " + request.responseCode);
+            } else
             {
                 Debug.Log("Response code: " + request.responseCode);
             }
+
         }
     }
-    // Start is called before the first frame update
-    void Start()
+    async void func()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        await TestWebsiteResponse(websites1);
+        await TestWebsiteResponse(websites2);
+        await TestWebsiteResponse(websites3);
     }
+   
+    
+    // Start is called before the first frame update
+       void Start()
+     {
+        stopButton.gameObject.SetActive(false);
+        errorButton.gameObject.SetActive(false);
+        actionStart += func;
+        startButton.onClick.AddListener(actionStart);
+     }
+
+     
 }
